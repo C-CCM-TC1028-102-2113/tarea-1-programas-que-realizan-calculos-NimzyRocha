5475c2764b512e390c79c75534d480d234c92c65
NimzyRocha <89158726+NimzyRocha@users.noreply.github.com>
2021-08-22 19:44:09 -0500

    Update and rename Telefono. to exercise.py

--- asignaciones/09Telefono/src/exercise.py
+++ asignaciones/09Telefono/src/exercise.py
@@ -1,13 +1,13 @@
 def main():
-print ('Dame el número de mensajes:')
+pprint ('Dame el número de mensajes:')
 mensajes= int(input())
-print ('Dame el número de megas:')
+pprint ('Dame el número de megas:')
 megas= float(input())
-print ('Dame el número de minutos:')
+pprint ('Dame el número de minutos:')
 minutos= int(input())
 minutos= round (minutos,2)
 costo=(mensajes*0.80)+(megas*0.80)+(minutos*0.80)
-print ('El costo mensual es:', costo)
+pprint ('El costo mensual es:', costo)
     pass
 
 
