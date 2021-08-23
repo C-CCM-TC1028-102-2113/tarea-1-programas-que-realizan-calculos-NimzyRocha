52552126b633bbd98850593bfc0d47b47ba16015
NimzyRocha <89158726+NimzyRocha@users.noreply.github.com>
2021-08-22 20:31:47 -0500

    Update and rename videojuegos. to exercise.py

---
+++ asignaciones/10 Videojuego/src/exercise.py
@@ -0,0 +1,13 @@
+def main():
+    #escribe tu código abajo de esta línea
+   pass
+print("Dame la cantidad de juegos nuevos:")
+nuevos=int(input())
+print("Dame la cantidad de juegos usados:")
+usados=int(input())
+costo=(nuevos*1000)+(usados*350)
+print("El total de la compra es:", costo)
+
+
+if __name__ == '__main__':
+    main()
