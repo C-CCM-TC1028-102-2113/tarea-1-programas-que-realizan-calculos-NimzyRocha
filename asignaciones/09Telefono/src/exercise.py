426c8ad830368389f2f188365be24a626ac36c95
NimzyRocha <89158726+NimzyRocha@users.noreply.github.com>
2021-08-22 20:33:04 -0500

    Update exercise.py

--- asignaciones/09Telefono/src/exercise.py
+++ asignaciones/09Telefono/src/exercise.py
@@ -1,13 +1,13 @@
 def main():
-pprint ('Dame el número de mensajes:')
+print ('Dame el número de mensajes:')
 mensajes= int(input())
-pprint ('Dame el número de megas:')
+print ('Dame el número de megas:')
 megas= float(input())
-pprint ('Dame el número de minutos:')
+print ('Dame el número de minutos:')
 minutos= int(input())
 minutos= round (minutos,2)
 costo=(mensajes*0.80)+(megas*0.80)+(minutos*0.80)
-pprint ('El costo mensual es:', costo)
+print ('El costo mensual es:', costo)
     pass
 
 
